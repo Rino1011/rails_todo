6b9859cc38d36dec8175996d219ce986e67b1930
Rino1011 <oy.06ls2@gmail.com>
2024-04-05 19:05:34 +0900

    dekita
    git push origin main
    quit

--- app/controllers/times_controller.rb
+++ app/controllers/times_controller.rb
@@ -11,4 +11,9 @@ class TimesController < ApplicationController
         Task.create(title: params[:title])
         redirect_to "/times"
     end
+    def destroy
+        @task = Task.find(params[:id])
+        @task.destroy
+        redirect_to "/times"
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   get "times" => "times#index"
   get "times/new" => "times#new"
   post "times/create" => "times#create"
+  get "times/:id" => "times#destroy"
 end
